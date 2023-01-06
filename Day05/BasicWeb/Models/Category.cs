7197836440d0c53b4d33f75b49848a4466f7d1bf
JongWon112 <gshowo131@daum.net>
2023-01-06 17:49:23 +0900

    이번주 마지막

---
+++ Day05/BasicWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicWeb.Models
+{
+    public class Category
+    {
+        [Key] //기본키
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public string DisplayOrder { get; set; }
+        public DateTime PostDate { get; set; } = DateTime.Now;
+
+    }
+}
